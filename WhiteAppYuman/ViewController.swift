052a403916e3b8657e8e18586b86a210a497eef2
Ann Yuman <annyman@MacBook-Pro-Ann.local>
2022-11-30 13:23:28 +0700

    результат урока stage1_lesson2

--- WhiteAppYuman/ViewController.swift
+++ WhiteAppYuman/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let me  = "humanBeing"
+    let meHight = "short"
+    let meSex = "female"
+    var myHairColor = "blond"
+    var myHairLenth = "short"
+    
+    let plantColor = "green"
+    var sizeOfThePlant = "small"
+    var plantPotSize = "big"
+    
+    let windowShape = "square"
+    var colorOfTheWindow = "white"
+    
+    let bagSize = "small"
+    var conditionOfTheBag = "opened"
+    
+    var sizeOfThePlantQuestion = true
+    var sizeOfThePlantHight: Float = 20.1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print(myHairColor)
+        print (me)
+        print(meSex)
+        print(colorOfTheWindow)
+    
     }
 
 
 }
+
+//let me string = "humanBeing"
+//let me = short
+//let me = female
+//var myHairColor = "blonde"
+//var myHair = "short"
+//
+//let plant = green
+//var sizeOfThePlant: String = small
+//var sizeOfThePlant: = big
+//
+//let window = square
+//var colorOfTheWindow = white
+//
+//let bag = small
+//var conditionOfTheBag = opened
+//
+//var sizeOfThePlant = true
+//var sizeOfThePlant = 20
 
