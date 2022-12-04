c55a19acc7360a567d2293f55e2cea8de6a05b1e
Ann Yuman <annyman@MacBook-Pro-Ann.local>
2022-12-04 17:28:01 +0700

    stage2_lesson1

--- WhiteAppYuman/ViewController.swift
+++ WhiteAppYuman/ViewController.swift
@@ -11,69 +11,51 @@ class ViewController: UIViewController {
     
     let r = 10
     var d = 6
-    lazy var h = r - d
+    var h = 0
+//        willSet (newValue) {
+//            print ("Значение h на данный момент принимает \(h)")
+//            print("Переменная h принимает значение \(newValue)" )
+//        }
+//        didSet {
+//            print("Переменная h принимает значение \(h)" )
+//
+//        }
+        
+        
+    
+    var name = "" {
+        didSet {
+            print ("Привет, " + name)
+        }
+    }
+    
     
     let (e, f) = (1, 2)
     
     let helloInChineese = "NiHao"
     let helloInRussian = "Privet"
-    
-    let hasDoorKey = false
-    let knowsOverridePassword = true
-    
-    let me = 158
-    var hasHighHeels = false
-    let highHeelsHeight: Int = 0
-    
-    
-    var calculation = true
-    var wrongCalculation = false
+
+    var razn: Int {
+        h - d
+    }
     
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    var razn2 = h - d
+    h = r - d
+    h = r + d
+    h = r * d
+    h = d - r
         
-        if hasDoorKey || knowsOverridePassword {
-            print ("Welcome!")
-        } else {
-            print ("ACCES DENIED")
-        }
-
-    calculation = false
-        if wrongCalculation || calculation {
-            print ("Good!")
-        } else {
-            print ("OK")
-        }
+    name = "Андрей"
+    name = "Аня"
         
-    hasHighHeels = true
-        if !hasHighHeels {
-            print ("You are short hah-hah!")
-        } else {
-            print ("You are tall!")
-        }
-
-        if h > 0 {
-            print ("You did it!")
-        } else {
-            print ("You are awsome!")
-        }
-        if h != d {
-            print ("It is becasue ten is not four.")
-        }
-        if r >= h {
-            print ("Access done!")
-        } else {
-            print ("Acsess denied.")
-        }
-        
-        if h != 4 {
-            print ("A mistake!")
-        } else {
-            print ("No mistake!")
-        }
+    print(razn)
+    print (razn2)
+    
     }
 
 
