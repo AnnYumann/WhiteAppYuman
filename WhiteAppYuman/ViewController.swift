ff91637ed42563c36133124d6fd370b5d7e751ea
Ann Yuman <annyman@MacBook-Pro-Ann.local>
2022-12-04 15:03:13 +0700

    исправленный результат урока stage1_lesson3

--- WhiteAppYuman/ViewController.swift
+++ WhiteAppYuman/ViewController.swift
@@ -11,58 +11,69 @@ class ViewController: UIViewController {
     
     let r = 10
     var d = 6
+    lazy var h = r - d
     
     let (e, f) = (1, 2)
     
     let helloInChineese = "NiHao"
     let helloInRussian = "Privet"
     
+    let hasDoorKey = false
+    let knowsOverridePassword = true
     
-//    let me = 158
-//    let hasHighHeels = true
-//    let highHeelsHeight: Int
-//    if hasHighHeels {
-//        highHeelsHeight = me + 15
-//    } else {
-//        highHeelsHeight = me + 0
-//    }
+    let me = 158
+    var hasHighHeels = false
+    let highHeelsHeight: Int = 0
+    
+    
+    var calculation = true
+    var wrongCalculation = false
+    
 
-//    let me = 158
-//    let hasHighHeels = true
-//    let highHeelsHeight + (hasHighHeels ? 15 : 0)
-//
-    
-//    let a = 3 + 5
-//    let b = 7 + 8
-//    let calculation = true
-//
-//    3 + 5 != 7 +8
-//    a != b
-//    a < b
-//    b > a
-//    b >= a
-//    a <= b
-//
-//    let wrongCalculation = a + (calculation ? 3 + 5 : 7 + 8)
-//
-    
-    
-    let openApp = false
-    if !openApp {
-        print("ERROR")
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    print ("World, " + helloInRussian)
         
-        if helloInChineese == "NiHao" {
-            print ("Lalala, NiHao")
+        if hasDoorKey || knowsOverridePassword {
+            print ("Welcome!")
         } else {
-            print( "I don't speak Chineese \(helloInChineese")
+            print ("ACCES DENIED")
         }
 
+    calculation = false
+        if wrongCalculation || calculation {
+            print ("Good!")
+        } else {
+            print ("OK")
+        }
+        
+    hasHighHeels = true
+        if !hasHighHeels {
+            print ("You are short hah-hah!")
+        } else {
+            print ("You are tall!")
+        }
+
+        if h > 0 {
+            print ("You did it!")
+        } else {
+            print ("You are awsome!")
+        }
+        if h != d {
+            print ("It is becasue ten is not four.")
+        }
+        if r >= h {
+            print ("Access done!")
+        } else {
+            print ("Acsess denied.")
+        }
+        
+        if h != 4 {
+            print ("A mistake!")
+        } else {
+            print ("No mistake!")
+        }
     }
 
 
